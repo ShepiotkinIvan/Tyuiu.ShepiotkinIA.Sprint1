edd6cc02a0516a409ef94c8e5f13f86ff92efdfd
Tyuiu.ShepiotkinIA@outlook.com <Tyuiu.ShepiotkinIA@outlook.com>
2023-10-14 16:14:29 +0500

    Task2

---
+++ Tyuiu.ShepiotkinIA.Sprint1.Task2.V22.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ShepiotkinIA.Sprint1.Task2.V22.Lib;
+
+
+namespace Tyuiu.ShepiotkinIA.Sprint1.Task2.V22.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            int y = 3;
+            int z = 4;
+            var res = ds.CalculateAVGValue(x, y, z);
+            Assert.AreEqual(3, res);
+        }
+    }
+}
---
+++ Tyuiu.ShepiotkinIA.Sprint1.Task2.V22/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShepiotkinIA.Sprint1.Task2.V22.Lib;
+
+namespace Tyuiu.ShepiotkinIA.Sprint1.Task2.V22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            int y;
+
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            int z;
+
+            Console.WriteLine("Введите значение Z:");
+            z = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Среднее значение X, Y, Z =" + ds.CalculateAVGValue(x, y, z));
+
+            Console.ReadLine();
+        }
+    }
+}
