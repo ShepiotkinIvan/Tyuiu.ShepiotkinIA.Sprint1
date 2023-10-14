0b0fa42f770d6a68491178d86c737c1312ce472b
Tyuiu.ShepiotkinIA@outlook.com <Tyuiu.ShepiotkinIA@outlook.com>
2023-10-14 16:31:14 +0500

    Task5

---
+++ Tyuiu.ShepiotkinIA.Sprint1.Task5.V2.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ShepiotkinIA.Sprint1.Task5.V2.Lib;
+
+
+namespace Tyuiu.ShepiotkinIA.Sprint1.Task5.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            double temp = 68;
+            DataService ds = new DataService();
+            double res = ds.FahrenheitToСelsius(temp);
+
+            int result = Convert.ToInt32(res);
+
+            int wait = 20;
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.ShepiotkinIA.Sprint1.Task5.V2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShepiotkinIA.Sprint1.Task5.V2.Lib;
+
+namespace Tyuiu.ShepiotkinIA.Sprint1.Task5.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите температуру по Фаренгейту: ");
+            double temp = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = Convert.ToInt32(ds.FahrenheitToСelsius(temp));
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
